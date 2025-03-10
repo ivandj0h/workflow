12d4054e0737602a4292c5ceb31b5bda4fb9091e
ivandjoh <ivandj0h@users.noreply.github.com>
2025-03-10 11:03:00 +0800

    working with main

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <section className="h-[calc(100vh-60px)] flex items-center justify-center">
+      <h2 className="text-2xl semibold">Dashboard</h2>
+      This is Dashboard Page
+    </section>
+  );
 };
 
 export default DashboardPage;
--- components/Shared/Topbar/Topbar.jsx
+++ components/Shared/Topbar/Topbar.jsx
@@ -1,9 +1,108 @@
+"use client";
+
 import React from "react";
+import { pathInfo } from "@/constants";
+import { usePathname } from "next/navigation";
+import { Bell } from "lucide-react";
+import { ModeToggle } from "@/components/ThemeSwitcher";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 const Topbar = () => {
+  const pathname = usePathname();
   return (
     <div className="w-full h-[60px] bg-background border-b flex items-center justify-between p-4">
-      Topbar
+      <div>
+        {pathname.includes("automation") ? (
+          <div>
+            <h3 className="text-lg font-semibold">
+              {pathname.includes("automation/")
+                ? "Workflow Builder"
+                : "Automation"}
+            </h3>
+            <p className="text-sm text-muted-foreground">
+              Automated Flow for Effective Actions
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h3 className="text-lg font-semibold">
+              {pathInfo[pathname.replace("/", "")].path}
+            </h3>
+            <p className="text-sm text-muted-foreground">
+              {pathInfo[pathname.replace("/", "")].subtitle}
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className="flex items-center gap-6">
+        <ModeToggle />
+
+        {/* Notification */}
+        <Popover>
+          <PopoverTrigger>
+            <span className="relative">
+              <Bell size={20} />
+              <span className="absolute -top-1 right-0 z-10">
+                <span className="relative flex h-2 w-2">
+                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-2 w-2 bg-sky-500"></span>
+                </span>
+              </span>
+            </span>
+          </PopoverTrigger>
+          <PopoverContent className="w-80" align="end">
+            Place content for the Notification
+          </PopoverContent>
+        </Popover>
+
+        {/* Account */}
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex items-center gap-3">
+            <Avatar className="w-8 h-8">
+              <AvatarImage
+                alt="profile-img"
+                src="https://github.com/shadcn.png"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>
+              <div>
+                <h3 className="text-base font-medium">ivandjoh</h3>
+                <p className="text-sm font-normal text-muted-foreground">
+                  ivandjoh@gmail.com
+                </p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>My Account</DropdownMenuItem>
+            <DropdownMenuItem>Subscriptions</DropdownMenuItem>
+            <DropdownMenuSeparator />
+
+            <div className="px-2 py-1.5">
+              <Button variant="destructive" className="w-full">
+                Sign out
+              </Button>
+            </div>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
