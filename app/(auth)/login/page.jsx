b7e217f721f84abc7a43e83df48dc0120924fa85
ivandjoh <ivandj0h@users.noreply.github.com>
2025-03-10 09:17:14 +0800

    login form created

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import LoginForm from "@/components/Auth/LoginForm";
+import { ModeToggle } from "@/components/ThemeSwitcher";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <section className="flex items-center justify-center h-screen">
+      <div className="fixed top-5 right-5">
+        <ModeToggle />
+      </div>
 
-export default LoginPage
+      <Card className="w-full rounded-lg shadow border border-input md:mt-0 sm:max-w-[32rem] xl:p-0">
+        <CardHeader>
+          <CardTitle>Create an Account</CardTitle>
+          <CardDescription>
+            Let's get started. No credit card needed.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+      </Card>
+    </section>
+  );
+};
+
+export default LoginPage;
